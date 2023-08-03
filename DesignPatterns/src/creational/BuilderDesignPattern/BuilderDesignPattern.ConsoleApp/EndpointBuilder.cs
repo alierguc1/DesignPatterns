bf03b32330d2ae49b03b1ec7291eab0aceacb1fa
Ali Ergüç <ergucali1@gmail.com>
2023-08-04 02:14:01 +0300

    Created Builder Design Pattern.

--- DesignPatterns/src/creational/BuilderDesignPattern/BuilderDesignPattern.ConsoleApp/EndpointBuilder.cs
+++ DesignPatterns/src/creational/BuilderDesignPattern/BuilderDesignPattern.ConsoleApp/EndpointBuilder.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace BuilderDesignPattern.ConsoleApp
 {
-    internal class EndpointBuilder
+    public class EndpointBuilder
     {
+        private readonly StringBuilder stringBuilderUrl = new();
+        private readonly StringBuilder stringBuilderParams = new();
+
+        private const char defaultDelimiter = '/';
+        private string BaseUrl { get; set; }
+
+        public EndpointBuilder(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+        public EndpointBuilder Append(string @value)
+        {
+            // localhost/api/v1/user
+            stringBuilderUrl.Append(@value);
+            //stringBuilderUrl.Append(defaultDelimiter);
+            return this;
+        }
+
+
+        /// <summary>
+        /// BaseUrl for Append parameter and value.
+        /// </summary>
+        /// <param name="key">parameter key</param>
+        /// <param name="value">parameter value</param>
+        /// <returns></returns>
+        public EndpointBuilder AppendParam(string key, string value)
+        {
+            // localhost/api/v1/user?id=5
+            stringBuilderUrl.AppendFormat("{0}={1}&", key, value);
+            return this;
+        }
+
+        public string Build()
+        {
+            if (BaseUrl.EndsWith(defaultDelimiter))
+                stringBuilderUrl.Insert(0, BaseUrl);
+            else
+                stringBuilderUrl.Insert(0, BaseUrl + defaultDelimiter);
+           
+            var url = stringBuilderUrl.ToString().TrimEnd('&');
+            
+            if(stringBuilderParams.Length > 0)
+            {
+                string queryParams = stringBuilderParams.ToString().TrimEnd('&');
+                url = stringBuilderUrl.ToString().TrimEnd(defaultDelimiter).TrimEnd('?');
+                url = $"{url}?{queryParams}";
+            }
+            
+            // localhost/api/v1/user?id=5
+  
+            return url.TrimEnd(defaultDelimiter);
+        }
     }
 }
--- DesignPatterns/src/creational/BuilderDesignPattern/BuilderDesignPattern.ConsoleApp/Program.cs
+++ DesignPatterns/src/creational/BuilderDesignPattern/BuilderDesignPattern.ConsoleApp/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BuilderDesignPattern.ConsoleApp;
+using System.Net;
 
-namespace BuilderDesignPattern.ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+var _endpointBuilder = new EndpointBuilder("https://localhost");
+
+_endpointBuilder
+    .Append("api")
+    .Append("v1")
+    .Append("user")
+    .AppendParam("id", "5")
+    .AppendParam("name", "ali");
+
+var _url = _endpointBuilder.Build();
+
+Console.WriteLine($"_endpointBuilder URL : {_url}");
+Console.ReadLine();
