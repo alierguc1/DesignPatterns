5ff515bb9f0ad962d65b1dfe267de5a5577fc230
Ali Ergüç <ergucali1@gmail.com>
2023-08-07 17:05:09 +0300

    Complete singleton Pattern.

--- DesignPatterns/src/creational/SingletonDesingPattern/SingletonDesingPattern.ConsoleApp/Program.cs
+++ DesignPatterns/src/creational/SingletonDesingPattern/SingletonDesingPattern.ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using SingletonDesingPattern.ConsoleApp;
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+
+//foreach (var country in countries)
+//{
+//    Console.WriteLine(country.Name);
+//}
+
+// Another service
+
+var countries1 = await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(CountryProvider.Instance.CountryCount);
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+
